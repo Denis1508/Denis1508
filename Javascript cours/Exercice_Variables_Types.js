09662566582e25ac3896d3752538bd898e5590f8
Denis1508 <145430984+Denis1508@users.noreply.github.com>
2023-09-22 12:23:42 +0200

    Add files via upload

---
+++ Javascript cours/Exercice_Variables_Types.js
@@ -0,0 +1,16 @@
+let Nombre_Avengers = 0; // number
+let Nom_famille_empereur_Jules = ""; // string
+let nombre_étoiles_univers = 0; // number
+let Message_bienvenue = ""; // string
+let Présence_face_ID = false; // bool
+let Prix_ordinateur = 0; // number
+let Noms_7_nains = ""; // string ou en faire un tableau
+let Profil_Tinder = ""; // string
+
+let Quantité_clones_Naruto = 0; //number
+let Défauts_embauche = "";//string
+let Prénom_Anne = "";//string
+let Panier_Amazon = "";//string
+let Durée_années_guerre_100_ans = 0;//number
+let Caractéristiques_biere = "";//string
+let Nom_prochain_président = "";//string
