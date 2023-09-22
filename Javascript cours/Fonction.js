ba7c5df2d0288b994a72487b1b20f08c566792fc
Denis1508 <145430984+Denis1508@users.noreply.github.com>
2023-09-22 10:38:59 +0200

    Add files via upload

---
+++ Javascript cours/Fonction.js
@@ -0,0 +1,12 @@
+let Nom1 = "NoobA";
+let Nom2 = "NoobB";
+let result=checkName(a,b)
+
+function checkName(Name1, Name2){
+    if(Name1===Name2){
+        return true
+    }else{
+        return false
+    }
+}
+console.log(result);
